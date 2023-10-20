75a37d11500f85ae9777addb2274af1abfa9624f
Mauricio Ospina <amauroo80@gmail.com>
2023-10-19 21:24:45 -0500

    Comentario de la clase conexion

--- src/configuracion/Conexion.java
+++ src/configuracion/Conexion.java
@@ -13,7 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Clase utilizada para conectar a una base de datos.
  * @author estefania
  */
 public class Conexion {
